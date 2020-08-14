8f56c2666fcd80302bca8dff73c9ae6e143a06ec
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-08-14 20:54:44 +0900

    Updates

---
+++ precache-manifest.3adb1c6d5845c5aa8617dabc37a56afe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3fc0a4890812a8177f9815af323d017",
+    "url": "/react-sangfilxx/index.html"
+  },
+  {
+    "revision": "c1b643c6c9b66111ea3f",
+    "url": "/react-sangfilxx/static/js/2.d3744bb5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-sangfilxx/static/js/2.d3744bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f1c49548edda157ef2",
+    "url": "/react-sangfilxx/static/js/main.d2930f20.chunk.js"
+  },
+  {
+    "revision": "4f275d33619dd29b1b28",
+    "url": "/react-sangfilxx/static/js/runtime-main.4941ae4e.js"
+  }
+]);
